0d22ca6c1d991158a0510a0dba54fddbf7f575b2
Raymond <devosray@gmail.com>
2016-07-14 23:02:10 +0200

    Working on testing recursive function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tuks_timetable_generator import web_scraper, configuration
-from tuks_timetable_generator.models import Choice, ChoiceListItem
+from tuks_timetable_generator.models import Choice, ChoiceListItem, ModuleFilterItem, LectureGroupingEnum,\
+    LectureLanguageEnum, Counter
 
 print("Downloading and parsing latest timetable...")
 module_list = web_scraper.download_and_parse_url(configuration.HATFIELD_TIMETABLE_URL)
@@ -7,20 +8,21 @@ module_list = web_scraper.download_and_parse_url(configuration.HATFIELD_TIMETABL
 print("Finished parsing timetable")
 
 # For testing purposes; demonstration list of modules to filter out
+# With this config there are 213840 options
 module_filter = [
-    "ALL 121",
-    "STK 120",
-    "WTW 124",
-    "COS 121",
-    "PHY 124",
-    "OBS 124",
-    "COS 110"
+    ModuleFilterItem("PHY 124"),
+    ModuleFilterItem("WTW 124"),
+    ModuleFilterItem("ALL 121"),
+    ModuleFilterItem("OBS 124"),
+    ModuleFilterItem("COS 121", lecture_grouping=LectureGroupingEnum.group_by_lecture_number),
+    ModuleFilterItem("COS 110", lecture_grouping=LectureGroupingEnum.group_by_lecture_number),
+    ModuleFilterItem("STK 120", lecture_langauge=)
 ]
 
 for filter in module_filter:
     found = False
     for module in module_list:
-        if module.module_name == filter:
+        if module.module_name == filter.module_name:
             found = True
             break
 
@@ -30,8 +32,13 @@ for filter in module_filter:
 # Remove modules not in filter list
 temp_list = []
 for module in module_list:
-    if module.module_name in module_filter:
-        temp_list.append(module)
+    found = False
+    for filter in module_filter:
+        if module.module_name == filter.module_name:
+            module.filter = filter
+            temp_list.append(module)
+            break
+
 module_list = temp_list
 
 # for module in module_list:
@@ -51,13 +58,95 @@ module_list = temp_list
 # Root node
 rootNode = Choice(parent=None, currentChoice=None)
 
+# Generate list of choices based on selected modules and preference
 choiceList = []
-item1 = ChoiceListItem(choices=['A', 'B', 'C'])
-item2 = ChoiceListItem(choices=['1', '2', '3'])
-item3 = ChoiceListItem(choices=['X', 'Y', 'Z'])
-choiceList.append(item1)
-choiceList.append(item2)
-choiceList.append(item3)
 
+def generate_grouped_by_group_choices_from_array(lecture_array):
+    group_choices = ChoiceListItem(choices=[])
+    lecture_groups = dict()
+    for lecture in lecture_array:
+        group = lecture.group
+        if group not in lecture_groups:
+            lecture_groups[group] = [lecture]
+        else:
+            lecture_groups[group].append(lecture)
+
+    for group_no in lecture_groups.keys():
+        lectures_array = lecture_groups[group_no]
+        group_choices.options.append(lectures_array)
+
+    return group_choices
+
+
+def generate_grouped_by_lecture_choices_from_array(lecture_array):
+    group_choices = ChoiceListItem(choices=[])
+    lecture_groups = dict()
+    for lecture in lecture_array:
+        group = lecture.lecture_number
+        if group not in lecture_groups:
+            lecture_groups[group] = [lecture]
+        else:
+            lecture_groups[group].append(lecture)
+
+    for group_no in lecture_groups.keys():
+        lectures_array = lecture_groups[group_no]
+        group_choices.options.append(lectures_array)
+
+    return group_choices
+
+
+for module in module_list:
+
+
+    # Split up in choices with the same lecture group like L1, L2 and L3 with common group 2
+    if module.filter.lecture_grouping == LectureGroupingEnum.group_by_group:
+        choiceList.append(generate_grouped_by_group_choices_from_array(module.lectures))
+
+    elif module.filter.lecture_grouping == LectureGroupingEnum.group_by_lecture_number:
+        generated_list = generate_grouped_by_lecture_choices_from_array(module.lectures)
+        for choice in generated_list.options:
+            new_choice_item = ChoiceListItem(choices=choice)
+            choiceList.append(new_choice_item)
+
+    # Filter tutorials
+    if module.filter.tutorial_grouping == LectureGroupingEnum.group_by_group:
+        choiceList.append(generate_grouped_by_group_choices_from_array(module.tutorials))
+
+    elif module.filter.tutorial_grouping == LectureGroupingEnum.group_by_lecture_number:
+        generated_list = generate_grouped_by_lecture_choices_from_array(module.tutorials)
+        for choice in generated_list.options:
+            new_choice_item = ChoiceListItem(choices=choice)
+            choiceList.append(new_choice_item)
+
+    # Select only ONE of all them
+    elif module.filter.tutorial_grouping == LectureGroupingEnum.group_by_type:
+        group_choices = ChoiceListItem(choices=[])
+        for tutorial in module.tutorials:
+            group_choices.options.append(tutorial)
+        choiceList.append(group_choices)
+
+    # Filter practicals
+    if module.filter.practical_grouping == LectureGroupingEnum.group_by_type:
+        group_choices = ChoiceListItem(choices=[])
+        for practical in module.practicals:
+            group_choices.options.append(practical)
+        choiceList.append(group_choices)
+
+# Filter out all empty choices
+temp_list = []
+for c in choiceList:
+    if len(c.options) > 0:
+        temp_list.append(c)
+choiceList = temp_list
+
+# item1 = ChoiceListItem(choices=[['A','P'], 'B', 'C'])
+# item2 = ChoiceListItem(choices=['1', '2', '3'])
+# item3 = ChoiceListItem(choices=['X', 'Y', 'Z'])
+# choiceList.append(item1)
+# choiceList.append(item2)
+# choiceList.append(item3)
+
+print("Generating Children")
 rootNode.generate_children(choiceList, 0)
 print("DONE")
+print(Counter.counter)
--- tuks_timetable_generator/models.py
+++ tuks_timetable_generator/models.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import time
 
 class TimetableEntry(object):
 
@@ -21,8 +23,18 @@ class TimetableEntry(object):
         self.lecture_number = lecture_number
         self.semester = semester
         self.day = day
-        self.time_start = time_start
-        self.time_end = time_end
+
+        # Parse time and catch exception (some entries does not have a time...????)
+        try:
+            self.time_start = time.strptime(self.day + time_start, '%A%H:%M:%S')
+        except:
+            self.time_start = None
+
+        try:
+            self.time_end = time.strptime(self.day + time_end, '%A%H:%M:%S')
+        except:
+            self.time_end = None
+
         self.venue = venue
 
     def __str__(self):
@@ -36,6 +48,29 @@ class TimetableEntry(object):
 
     def is_practical(self):
         return self.lecture_number[0] == 'P'
+
+    def does_clash_with_other(self, other_entry):
+        clash = False
+
+        if other_entry.day != self.day:
+            return False
+
+        if (other_entry.time_start == self.time_start and other_entry.time_end == self.time_end):
+            clash = True
+
+        if (self.time_start < other_entry.time_start < self.time_end):
+            clash = True
+
+        if (self.time_start < other_entry.time_end < self.time_end):
+            clash = True
+
+        if (other_entry.time_start > self.time_start and other_entry.time_end < self.time_end):
+            clash = True
+
+        if (self.time_start > other_entry.time_start and self.time_end < other_entry.time_end):
+            clash = True
+
+        return clash
 
 
 class Module(object):
@@ -51,6 +86,8 @@ class Module(object):
 
         self.module_name = name
         self.semester = semester
+
+        self.filter = ModuleFilterItem(self.module_name)
 
     def __str__(self):
         return "%s sem. %s: %s lectures, %s tuts and %s pracs" % (self.module_name, self.semester,
@@ -76,22 +113,79 @@ class Choice(object):
     def generate_children(self, choice_list, choice_index):
         if len(choice_list) <= choice_index:
             # Leaf node. Time to navigate back up
-            print(self.temp_find_root())
+            #print(self.temp_find_root())
+            entry_array = self.get_all_chosen_entries()
+
+            # print(len(entry_array))
+
+            # Group into days
+            entries_grouped = dict()
+            for entry in entry_array:
+                if entry.day in entries_grouped:
+                    entries_grouped[entry.day].append(entry)
+                else:
+                    entries_grouped[entry.day] = [entry]
+            # print("FOUND :'D")
+            Counter.counter += 1
             return None
         else:
+            # Before we check any more children, we need to
+
             choice_item = choice_list[choice_index]
             choice_item.reset_current_option()
             while choice_item.has_more():
                 new_child = Choice(self, choice_item.get_next_choice())
                 self.children.append(new_child)
-                new_index = choice_index + 1
-                new_child.generate_children(choice_list, new_index)
+
+                # Before we add the new node to the tree, we need to make sure it fits!
+                is_valid = True
+                if isinstance(new_child.currentChoice, list):
+                    for new_current_choice in new_child.currentChoice:
+                        if not self.is_valid_child(new_current_choice):
+                            is_valid = False
+                else:
+                    if not self.is_valid_child(new_child.currentChoice):
+                        is_valid = False
+
+                if is_valid:
+                    new_index = choice_index + 1
+                    new_child.generate_children(choice_list, new_index)
+                    self.children = []
 
     def temp_find_root(self):
         if self.parent == None:
             return ""
         else:
             return self.currentChoice + self.parent.temp_find_root()
+
+    def get_all_chosen_entries(self):
+        if self.currentChoice is None:
+            return []
+        else:
+            arr = self.parent.get_all_chosen_entries()
+            if isinstance(self.currentChoice, list):
+                for choice in self.currentChoice:
+                    arr.append(choice)
+            else:
+                arr.append(self.currentChoice)
+            return arr
+
+    # Recursive function to see if a newly generated child clashes
+    def is_valid_child(self, new_child):
+        if self.parent == None:
+            # No parent means root tree and that makes it automatically a valid entry
+            return True
+        else:
+            # Does the time overlap with any of the choices?
+            if isinstance(self.currentChoice, list):
+                for choice in self.currentChoice:
+                    if choice.does_clash_with_other(new_child):
+                        return False
+            else:
+                if self.currentChoice.does_clash_with_other(new_child):
+                    return False
+            return self.parent.is_valid_child(new_child)
+
 
 
 class ChoiceListItem(object):
@@ -112,3 +206,34 @@ class ChoiceListItem(object):
 
     def reset_current_option(self):
         self.currentOption = -1
+
+
+class LectureGroupingEnum(Enum):
+    group_by_group = 1
+    group_by_lecture_number = 2
+    group_by_type = 3
+
+class LectureLanguageEnum(Enum):
+    afrikaans = 1
+    english = 2
+    any = 3
+
+class ModuleFilterItem(object):
+
+    def __init__(self, module_name,
+                 lecture_grouping=LectureGroupingEnum.group_by_group,
+                 tutorial_grouping=LectureGroupingEnum.group_by_type,
+                 practical_grouping=LectureGroupingEnum.group_by_type,
+                 lecture_langauge=LectureLanguageEnum.any):
+        self.lecture_grouping = lecture_grouping
+        self.tutorial_grouping = tutorial_grouping
+        self.practical_grouping = practical_grouping
+        self.module_name = module_name
+        self.lecture_language = lecture_langauge
+
+    def __str__(self):
+        return self.module_name
+
+class Counter(object):
+    counter = 0
+
